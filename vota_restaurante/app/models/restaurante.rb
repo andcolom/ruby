3815fc7c43cafcd1c5a8b0e5dee3cac2fe9404b7
andre.120287 <andre.colombano@gmail.com>
2014-08-15 17:15:30 -0300

    Curso caelum

--- vota_restaurante/app/models/restaurante.rb
+++ vota_restaurante/app/models/restaurante.rb
@@ -1,2 +1,23 @@
 class Restaurante < ActiveRecord::Base
+
+validates_presence_of :nome, message: "deve ser preenchido"
+validates_presence_of :endereco, message: "deve ser preenchido"
+validates_presence_of :especialidade, message: "deve ser preenchido"
+validates_uniqueness_of :nome, message: "nome ja cadastrado"
+validates_uniqueness_of :endereco, message: "endereco ja cadastrado"
+
+validate :primeira_letra_deve_ser_maiuscula
+
+private
+def primeira_letra_deve_ser_maiuscula
+  errors.add("nome","primeira letra deve ser maiuscula") unless nome =~ /[A-Z].*/
+end
+
+
+#validates :nome, presence: true, uniqueness: true, length: {maximum: 50}
+#equivalente a:
+#validates_presence_of :nome
+#validates_uniqueness_of :nome
+#validates_length_of :nome, :maximum => 50
+
 end
